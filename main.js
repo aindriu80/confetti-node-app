c09f71eb05b31989c4ba5dbf7948510cb9ec2a09
aindriu80 <aindriu80@gmail.com>
2020-11-28 16:04:59 +0000

    MongoDB setup

--- main.js
+++ main.js
@@ -3,7 +3,33 @@
 const express = require("express"),
   app = express(),
   homeController = require("./controllers/homeController"),
-  errorController = require("./controllers/errorController");
+  errorController = require("./controllers/errorController"),
+  // layouts = require("express-ejs-layouts"),
+  MongoDB = require("mongodb").MongoClient,
+  dbURL = "mongodb://localhost:27017",
+  dbName = "recipe_db";
+
+MongoDB.connect(dbURL, (error, client) => {
+  if (error) throw error;
+  let db = client.db(dbName);
+  db.collection("contacts")
+    .find()
+    .toArray((error, data) => {
+      if (error) throw error;
+      console.log(data);
+    });
+
+  db.collection("contacts").insert(
+    {
+      name: "Freddie Mercury",
+      email: "fred@queen.com",
+    },
+    (error, db) => {
+      if (error) throw error;
+      console.log(db);
+    }
+  );
+});
 
 app.set("port", process.env.PORT || 3000);
 
